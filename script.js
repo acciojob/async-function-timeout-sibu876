53d8b536f03d6b5161c30329cae7d409d17121df
Sibghatullah <145217999+sibu876@users.noreply.github.com>
2026-07-23 21:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+
+async  function call(){
+	const delay = document.getElementById("delay").value;
+  const text = document.getElementById("text").value;
+    
+	await new Promise((resolve) =>{
+	     setTimeout(()=>{
+			output.textContent = text;
+			 resolve();
+		},delay * 1000)
+	})
+	
+}
+
+btn.addEventListener('click',() =>{
+		call();
+	})
